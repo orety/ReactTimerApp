b27618c7ab08b8dc6ca76fd281bf29552a60f079
Patryk Seredynski <patryk.seredynski@gamesys.co.uk>
2017-09-15 21:06:05 +0100

    Lecture 72. Clock Component Part 2

--- app/components/Clock.jsx
+++ app/components/Clock.jsx
@@ -1,6 +1,14 @@
 var React = require('react');
 
 var Clock = React.createClass({
+  getDefaultProps: function () {
+    totalSeconds: 0
+  },
+
+  propTypes: {
+    totalSeconds: React.PropTypes.number
+  },
+
   formatSeconds: function (totalSeconds) {
     var seconds = totalSeconds % 60;
     var minutes = Math.floor(totalSeconds / 60); //rounds down
@@ -12,12 +20,19 @@ var Clock = React.createClass({
     if(minutes < 10) {
       minutes =  '0' + minutes;
     }
+    // console.log('formatSeconds, minutes: ' + minutes);
+    // console.log('formatSeconds, seconds: ' + seconds);
+    // console.log('formatSeconds, minutes + ':' + seconds: ', minutes + ':' + seconds);
     return minutes + ':' + seconds;
   },
-  render: function () {
-    return (
-      <div>
 
+  render: function () {
+    var {totalSeconds} = this.props  //es6 descructuring
+    return (
+      <div className = "clock">
+        <span className="clock-text">
+          {this.formatSeconds(totalSeconds)}
+        </span>
       </div>
     )
   }
--- app/tests/components/clock.test.jsx
+++ app/tests/components/clock.test.jsx
@@ -8,8 +8,16 @@ var Clock = require('Clock');
 
 describe('Clock', () => {
   it('should exist', () => {
-    // expect(Clock).toExist();
-    expect(1).toBe(1);
+    expect(Clock).toExist();
+});
+
+  describe(' render', () => {
+    it('should render clock to output', () => {
+      var clock = TestUtils.renderIntoDocument(<Clock totalSeconds = {62}/>);
+      var $el = $(ReactDOM.findDOMNode(clock));
+      var actualText = $el.find('.clock-text').text();
+      expect(actualText).toBe('01:02');
+    });
   });
 });
 
